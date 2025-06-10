096545732b482ad88482cbb4eb9572bf9b910649
oJohnnykkk <jpedro.costa1218@gmail.com>
2025-06-10 13:39:55 -0300

    Movi os arquivos para a raiz do projeto

--- app.py
+++ app.py
@@ -5,8 +5,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    data_inicio = datetime(2023, 5, 27, 0, 0, 0)  # com hora zero
-    return render_template("index.html", data_inicio=data_inicio.isoformat())
+    inicio = datetime(2023, 5, 27, 0, 0, 0)
+    agora = datetime.now()
+    delta = agora - inicio
+
+    anos = delta.days // 365
+    meses = (delta.days % 365) // 30
+    dias = (delta.days % 365) % 30
+    horas = delta.seconds // 3600
+    minutos = (delta.seconds % 3600) // 60
+    segundos = delta.seconds % 60
+
+    return render_template("index.html",
+                           anos=anos,
+                           meses=meses,
+                           dias=dias,
+                           horas=horas,
+                           minutos=minutos,
+                           segundos=segundos)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
